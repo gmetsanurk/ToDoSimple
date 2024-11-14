ce6fa70b89dd9b3ff24abc331a7e4362e77cc901
Georgy Metsanurk <gmetsanurk@gmail.com>
2024-11-14 11:29:38 +0300

    initial commit

---
+++ ToDoSimple/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        setupWindow()
+        return true
+    }
+
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
