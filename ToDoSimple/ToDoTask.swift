efdf2ef686d67b54a100c0f4d66ec412a027b6fe
Georgy Metsanurk <gmetsanurk@gmail.com>
2024-11-17 14:33:44 +0300

    create ToDoManager

--- ToDoSimple/ToDoTask.swift
+++ ToDoSimple/ToDoTask.swift
@@ -1,4 +1,17 @@
-struct ToDoTask {
+import Foundation
+
+/*struct ToDoTask {
     var title: String
     var isCompleted: Bool
+}*/
+
+struct ToDoTask: Codable {
+    let id: Int
+    let todo: String
+    let completed: Bool
+    let userId: Int
+}
+
+struct ToDoResponse: Codable {
+    let todos: [ToDoTask]
 }
