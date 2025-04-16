27d32863d9a90d1a9f691dcf9bbaa78f039b0723
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-04-16 09:13:00 +0300

    Implement lozacizable strings

--- ToDoSimple/Coordinator.swift
+++ ToDoSimple/Coordinator.swift
@@ -50,14 +50,19 @@ struct UIKitCoordinator: Coordinator {
     }
     
     func openAlert(onAdd: @escaping (String?) -> Void) {
-        let alert = UIAlertController(title: "New Task", message: "Enter task title", preferredStyle: .alert)
+        let newTaskTitle = NSLocalizedString("home_screen.new_task_title", comment: "New task alert controller")
+        let newTaskMessageBody = NSLocalizedString("home_screen.new_task_message", comment: "New task message body")
+        let addTitle = NSLocalizedString("home_screen.new_task_add", comment: "New task add message")
+        let cancelTitle = NSLocalizedString("home_screen.new_task_cancel", comment: "New task cancel message")
+        
+        let alert = UIAlertController(title: newTaskTitle, message: newTaskMessageBody, preferredStyle: .alert)
         alert.addTextField()
         
-        let addAction = UIAlertAction(title: "Add", style: .default) { _ in
+        let addAction = UIAlertAction(title: addTitle, style: .default) { _ in
             onAdd(alert.textFields?.first?.text)
         }
         
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel)
         alert.addAction(addAction)
         alert.addAction(cancelAction)
         
